40f8719de53badbae47c8f0790acb6321b8d8ea8
Eli <elisego45@gmail.com>
2020-10-29 18:26:18 -0500

    Add Landing styling

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -3,15 +3,19 @@ import './Landing.css';
 export default function Landing() {
     return (
         <div className='Landing'>
-            <div className='Landing-content d-flex justify-content-around align-items-center'>
-                <div className='Landing-col col1'>
+            <div className='Landing-content d-flex flex-md-row flex-column justify-content-around align-items-center'>
+                <div className='Landing-col col1 d-flex flex-column justify-content-center align-items-center'>
                     <ul>
                         <li>Lorem ipsum dolor sit.</li>
                         <li>Lorem, ipsum dolor.</li>
                         <li>Lorem ipsum dolor sit amet.</li>
                     </ul>
                 </div>
-                <div className='Landing-col col2 bg-smu-blue'>hello</div>
+                <div className='Landing-col col2 d-flex flex-column justify-content-center align-items-center'>
+                    <div className='Landing-form-container'>
+                        <form className='Landing-login-form'></form>
+                    </div>
+                </div>
             </div>
         </div>
     );
