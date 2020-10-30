d17a1785e6869ab7f294f6d38b207953d3d1ed07
Eli <elisego45@gmail.com>
2020-10-29 19:36:49 -0500

    Add logo

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Landing from './Landing';
 
 function App() {
-    return <div className='App bg-primary'></div>;
+    return (
+        <div className='App'>
+            <Landing />
+        </div>
+    );
 }
 
 export default App;
