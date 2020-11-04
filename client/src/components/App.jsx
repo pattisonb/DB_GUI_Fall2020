0c1947f70d6b8e69b764ea34061188d5b4f8ddc1
Eli <elisego45@gmail.com>
2020-11-03 18:24:18 -0600

    work on messaging

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
 import Landing from './Landing';
+import Register from './Register';
+import Messaging from './Messaging';
 
 function App() {
     return (
         <div className='App'>
-            <Landing />
+            <Messaging />
         </div>
     );
 }
