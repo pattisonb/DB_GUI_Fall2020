d72378aff912fa0210c164385f19619de6ccfbc8
Eli <elisego45@gmail.com>
2020-11-04 13:56:02 -0600

    Add Register page functionality

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,29 +1,47 @@
 import React from 'react';
 import './Register.css';
 import PonyListText from '../img/PonyList-Text.png';
-export default function Register() {
+export default function Register(props) {
     return (
         <div className='Register'>
-            <img className='Register-PonyList' src={PonyListText} alt='' />
-            <form>
+            <div className='icon d-flex w-100 justify-content-end'>
+                <i
+                    onClick={() => {
+                        document
+                            .querySelector('.Register')
+                            .classList.add('leave');
+
+                        setTimeout(() => props.onClose(), 800);
+                    }}
+                    className='fas fa-window-close fa-2x mr-3'
+                ></i>
+            </div>
+            <h2>Register</h2>
+            <form className='Register-form mt-5'>
                 <div className='d-flex flex-column justify-content-center align-items-center p-2'>
-                    <label htmlFor='reg-username'></label>
+                    <label className='align-self-start' htmlFor='reg-username'>
+                        Username
+                    </label>
                     <input
-                        className='form-control-lg w-100 m-2'
+                        className='form-control-lg w-100 mt-2 mb-3'
                         type='text'
+                        required
                         name='reg-username'
                         id='reg-username'
                     />
+                    <label className='align-self-start' htmlFor='reg-password'>
+                        Password
+                    </label>
                     <input
-                        className='form-control-lg w-100 m-2'
+                        className='form-control-lg w-100 mb-2'
+                        required
                         type='password'
                         name='reg-password'
                         id='reg-password'
                     />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Esse saepe, molestiae nihil iure quas itaque harum nobis
-                    reiciendis quam ex repudiandae! Laborum soluta esse minima
-                    facilis praesentium quibusdam repudiandae quae.
+                    <button type='button' className='mt-5 btn btn-block'>
+                        Register
+                    </button>
                 </div>
             </form>
         </div>
