e66bbb51e4b596942a12a39b36aec45209d441a8
Eli <elisego46@gmail.com>
2020-11-19 13:14:34 -0600

    Add chat stuff

--- frontend/src/components/messaging/Contacts.jsx
+++ frontend/src/components/messaging/Contacts.jsx
@@ -12,30 +12,42 @@ export const Contacts = () => {
     return this;
   };
   const [contacts, setContacts] = useState([]);
+
+  const fetchContacts = async () => {
+    let contacts;
+    await axios
+      .get(
+        `http://localhost:8000/contacts/${window.localStorage.getItem('id')}`
+      )
+      .then((res) => {
+        contacts = [...new Set(res.data.map((contact) => contact.RecipientID))];
+      })
+      .catch((err) => {
+        return err;
+      });
+    console.log(contacts);
+    return contacts;
+  };
+
   useEffect(() => {
-    //axios.get("http://localhost:8000/stuff")
-    setContacts([
-      { id: 100, name: 'Bob' },
-      { id: 200, name: 'Jack' },
-      { id: 300, name: 'zack' },
-    ]);
+    fetchContacts().then((res) => setContacts(res));
   }, []);
   return (
     <div>
       <ul>
         {contacts.map((contact) => {
           return (
-            <li key={contact.id}>
+            <li key={contact}>
               <Link
                 to={`/chat?name=${window.localStorage.getItem('id')}&room=${[
-                  0,
+                  `${window.localStorage.getItem('id')}`,
                   '-',
-                  contact.id,
+                  contact,
                 ]
                   .swap(0, 2)
                   .join('')}`}
               >
-                {contact.name}
+                {contact}
               </Link>
             </li>
           );
