34ddf954f6a8b054345d35404426ab1a0a24ddd2
Eli <elisego45@gmail.com>
2020-11-25 21:55:18 -0600

    Work on managing items

---
+++ frontend/src/components/Items/CurrentItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import DetailNav from '../layout/DetailNav';
+export const CurrentItems = () => {
+    return (
+        <div className='container mt-4'>
+            <DetailNav />
+        </div>
+    );
+};
---
+++ frontend/src/components/Items/PastItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DetailNav from '../layout/DetailNav';
+const PastItems = () => {
+    return (
+        <div className='container mt-4'>
+            <DetailNav />
+        </div>
+    );
+};
+
+export default PastItems;
