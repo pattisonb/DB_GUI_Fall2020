4824c2592072ab80765e0185ab98c5098fbbba70
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 15:27:57 -0600

    made profilePage more responsive

--- frontend/src/components/profile/ProfilePage.jsx
+++ frontend/src/components/profile/ProfilePage.jsx
@@ -59,161 +59,185 @@ export class ProfilePage extends React.Component {
             return <div>Loading Home...</div>;
         }
         return (
-            <div className='ProfilePage p-4'>
+            <div className='ProfilePage p-5'>
                 <DetailNav />
-            <div className='container master-container m-0 p-0'>
-            <div className='jumbotron bg-smu-blue-home m-0'>
-            <div className='side-nav-bar-img-box'>
-                <img src={this.state.user.ProfilePicture} alt='profile-img' width='150px' height='150px' />
+                <div className="profileHeader bg-smu-blue-home">
+                    <div className="row p-3">
+                        <div className="col-sm-6">
+                            <div class="row">
+                                <div class="col-sm-auto">
+                                    <img src={this.state.user.ProfilePicture} alt='profile-img' width='150px' height='150px' />
+                                </div>
+                                <div class="col-sm-auto">Name</div>
+                                <div class="w-100"></div>
+                                <div class="col-sm-auto">
+                                    {this.state.id == window.localStorage.getItem('id')
+                                        ? 
+                                            <button type="button" class="btn btn-secondary btn-sm" onClick={() => {this.changeProfileImage()}}>Change img</button>
+                                        :
+                                            <Link
+                                            to={`/chat?name=${window.localStorage.getItem(
+                                                'id'
+                                            )}&room=${[
+                                                    `${window.localStorage.getItem('id')}`,
+                                                    '-',
+                                                    this.state.user.UserID,
+                                                ]
+                                                    .swap(0, 2)
+                                                    .join('')}`}
+                                                className='btn btn-primary btn-sm mt-2'
+                                            >
+                                                Contact User
+                                            </Link>
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm">
+                        <table className='tablew-50'>
+                            <thead></thead>
+                            <tbody>
+                                <tr>
+                                    <th>Rating</th>
+                                    <td><Rating value={this.state.userRating.Rating} /></td>
+                                </tr>
+                                <tr>
+                                    <th>Location</th>
+                                    <td>{this.state.user.Location}</td>
+                                </tr>
+                                {this.state.user.OnCampus === "YES" &&
+                                <tr>
+                                    <th>Dorm</th>
+                                    <td>{this.state.user.Dorm}</td>
+                                </tr>}
+                                <tr>
+                                    <th>Miles Away</th>
+                                    <td>{this.state.user.MilesAway}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
                 </div>
-                {this.state.selfId === this.state.user.UserID &&
-                    <button type="button" class="btn btn-secondary btn-sm" onClick={() => {this.changeProfileImage()}}>Change img</button>
-                }
-                <h1 className='text-align-center'>
-                {this.state.user.Username}
-                </h1>
-                <div className='logo-separator'></div>
-                <table className='tablew-50'>
-                        <thead></thead>
-                        <tbody>
-                            <tr>
-                                <th>Rating</th>
-                                <td><Rating value={this.state.userRating.Rating} /></td>
-                            </tr>
-                            <tr>
-                                <th>Location</th>
-                                <td>{this.state.user.Location}</td>
-                            </tr>
-                            {this.state.user.OnCampus === "YES" &&
-                            <tr>
-                                <th>Dorm</th>
-                                <td>{this.state.user.Dorm}</td>
-                            </tr>}
-                            <tr>
-                                <th>Miles Away</th>
-                                <td>{this.state.user.MilesAway}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    {this.state.id == window.localStorage.getItem('id')
-                            ? 
+                
+                    {/*<div className='jumbotron bg-smu-blue-home m-0'>
+                        
+                        <h1 className='text-align-center'>
+                            {this.state.user.Username}
+                        </h1>
+                        <div className='logo-separator'></div>
+                        
+                        {this.state.id == window.localStorage.getItem('id')
+                                ? 
+                                    <Link
+                                        className='btn mx-auto mt-5 w-75 btn-block btn-primary'
+                                        to={`/sellItems/${this.state.id}`}
+                                    >
+                                        {' '}
+                                        Creat a new listing!{' '}
+                                    </Link>
+                                :
                                 <Link
-                                    className='btn mx-auto mt-5 w-75 btn-block btn-primary'
-                                    to={`/sellItems/${this.state.id}`}
-                                >
-                                    {' '}
-                                    Creat a new listing!{' '}
-                                </Link>
-                            :
-                            <Link
-                            to={`/chat?name=${window.localStorage.getItem(
-                                'id'
-                            )}&room=${[
-                                `${window.localStorage.getItem('id')}`,
-                                '-',
-                                this.state.user.UserID,
-                            ]
-                                .swap(0, 2)
-                                .join('')}`}
-                            className='btn mx-auto mt-5 w-75 btn-block btn-chat'
-                        >
-                            Contact User
-                        </Link>
-                    }
-                    
-
-                    
-            </div>
-
-            <div className="container emp-profile">
-                <ul className="nav nav-tabs p-0 m-0" id="myTab" role="tablist">
-                    <li className="nav-item">
-                    <a className="nav-link active" id="reviews-tab" data-toggle="tab" href="#reviews" role="tab" aria-controls="reviews" aria-selected="true"><h4 id="text">Reviews({this.state.reviews.length})</h4></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" id="listings-tab" data-toggle="tab" href="#listings" role="tab" aria-controls="listings" aria-selected="false"><h4 id="text">Listings({this.state.listingCount})</h4></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" id="pastSales-tab" data-toggle="tab" href="#pastSales" role="tab" aria-controls="pastSales" aria-selected="false"><h4 id="text">Past Sales({this.state.salesCount})</h4></a>
-                    </li>
-                    {this.state.selfId === this.state.user.UserID &&
-                    <>
-                    <li className="nav-item">
-                        <a className="nav-link" id="favoriteItems-tab" data-toggle="tab" href="#favoriteItems" role="tab" aria-controls="favoriteItems" aria-selected="false"><h4 id="text">Favorited Items({this.state.favoriteItems.length})</h4></a>
-                    </li>
-                    </>
-                    }
-                </ul>
-                <div className="tab-content profile-tab">
-                    <div className="tab-pane fade show active" id="reviews" role="tabpanel" aria-labelledby="reviews-tab">
-                    {this.state.reviews.length === 0 && (
-                        <>
-                        <h2 className='text-center display-3'>
-                            {this.state.id == window.localStorage.getItem('id')
-                                ? "You don't have"
-                                : "This seller doesn't have"}{' '}
-                        </h2>
-                        <h2 className='text-center display-3'>
-                            any reviews yet.
-                        </h2>
-                        </>
-                    )}
-                    {
-                    this.state.reviews.map((review, idx) =>
-                        <article key={idx} className="card mb-4">
-                        <div className="card-header">
-                            <Rating value={ review.Rating } />
-                        </div>
-                        <div className="card-body">
-                            <p className="text-secondary">{ review.Username }
-                            <span className="float-right">{ new Date(review.Date).getFullYear() }-{ new Date(review.Date).getMonth() + 1 }-{ new Date(review.Date).getDate() }</span>
-                            </p>
-                            <p>"{ review.ReviewText }"</p>
-                        </div>
-                        </article>)
-                    }
-                    </div>
-                    <div className="tab-pane fade" id="listings" role="tabpanel" aria-labelledby="listings-tab">
-                        <CurrentItems id={this.state.id}/>
-                    </div>
-                    <div className="tab-pane fade" id="pastSales" role="tabpanel" aria-labelledby="pastSales-tab">
-                        <PastItems id={this.state.id}/>
-                    </div>
-                    <div className="tab-pane fade" id="favoriteItems" role="tabpanel" aria-labelledby="favoriteItems-tab">
-                    {this.state.favoriteItems.length === 0 && (
-                        <h2 className='display-3 mt-5 text-center'>
-                            No items to show...
-                        </h2>
-                    )}
-                    {
-                    this.state.favoriteItems.map((product, idx) =>
-                        <article key={idx} className="card mb-4">
-                        <div className="card-header">
-                            <Link to={`/products/${product.ItemID}`}>
-                            {product.ItemName}
+                                to={`/chat?name=${window.localStorage.getItem(
+                                    'id'
+                                )}&room=${[
+                                    `${window.localStorage.getItem('id')}`,
+                                    '-',
+                                    this.state.user.UserID,
+                                ]
+                                    .swap(0, 2)
+                                    .join('')}`}
+                                className='btn mx-auto mt-5 w-75 btn-block btn-chat'
+                            >
+                                Contact User
                             </Link>
+                        }  
+                    </div>*/}
+
+                <div className="mx-auto">
+                    <ul className="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+                        <li className="nav-item">
+                            <a className="nav-link active" id="reviews-tab" data-toggle="tab" href="#reviews" role="tab" aria-controls="reviews" aria-selected="true"><h4 id="text">Reviews({this.state.reviews.length})</h4></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="listings-tab" data-toggle="tab" href="#listings" role="tab" aria-controls="listings" aria-selected="false"><h4 id="text">Listings({this.state.listingCount})</h4></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="pastSales-tab" data-toggle="tab" href="#pastSales" role="tab" aria-controls="pastSales" aria-selected="false"><h4 id="text">Past Sales({this.state.salesCount})</h4></a>
+                        </li>
+                        {this.state.selfId === this.state.user.UserID &&
+                        <li className="nav-item">
+                            <a className="nav-link" id="favoriteItems-tab" data-toggle="tab" href="#favoriteItems" role="tab" aria-controls="favoriteItems" aria-selected="false"><h4 id="text">Favorites({this.state.favoriteItems.length})</h4></a>
+                        </li>
+                        }
+                    </ul>
+                    <div className="tab-content profile-tab">
+                        <div className="tab-pane fade show active pt-5" id="reviews" role="tabpanel" aria-labelledby="reviews-tab">
+                            {this.state.reviews.length === 0 && (
+                                <>
+                                <h2 className='text-center display-3'>
+                                    {this.state.id == window.localStorage.getItem('id')
+                                        ? "You don't have"
+                                        : "This seller doesn't have"}{' '}
+                                </h2>
+                                <h2 className='text-center display-3'>
+                                    any reviews yet.
+                                </h2>
+                                </>
+                            )}
+                            {this.state.reviews.map((review, idx) =>
+                                <article key={idx} className="card mb-4">
+                                <div className="card-header">
+                                    <Rating value={ review.Rating } />
+                                </div>
+                                <div className="card-body">
+                                    <p className="text-secondary">{ review.Username }
+                                    <span className="float-right">{ new Date(review.Date).getFullYear() }-{ new Date(review.Date).getMonth() + 1 }-{ new Date(review.Date).getDate() }</span>
+                                    </p>
+                                    <p>"{ review.ReviewText }"</p>
+                                </div>
+                                </article>)
+                            }
                         </div>
-                        <div className="card-body">
-                        <Link to={`/products/${product.ItemID}`}>
-                            <img
-                            src={product.ImageURL}
-                            alt='product-image'
-                            width='100px'
-                            height='100px'
-                            className='ml-3 mr-2'
-                            />
-                        </Link>
-                            <p className="text-secondary">
-                            <span className="float-right">{ new Date(product.DatePosted).getFullYear() }-{ new Date(product.DatePosted).getMonth() + 1 }-{ new Date(product.DatePosted).getDate() }</span>
-                            </p>
-                            <p>"{ product.ItemDetails }"</p>
+                        <div className="tab-pane fade" id="listings" role="tabpanel" aria-labelledby="listings-tab">
+                            <CurrentItems id={this.state.id}/>
                         </div>
-                        </article>)
-                }
+                        <div className="tab-pane fade" id="pastSales" role="tabpanel" aria-labelledby="pastSales-tab">
+                            <PastItems id={this.state.id}/>
+                        </div>
+                        <div className="tab-pane fade pt-5" id="favoriteItems" role="tabpanel" aria-labelledby="favoriteItems-tab">
+                            {this.state.favoriteItems.length === 0 && (
+                                <h2 className='display-3 mt-5 text-center'>
+                                    No items to show...
+                                </h2>
+                            )}
+                            {this.state.favoriteItems.map((product, idx) =>
+                                <article key={idx} className="card mb-4">
+                                    <div className="card-header">
+                                        <Link to={`/products/${product.ItemID}`}>
+                                            {product.ItemName}
+                                        </Link>
+                                    </div>
+                                    <div className="card-body">
+                                        <Link to={`/products/${product.ItemID}`}>
+                                            <img
+                                            src={product.ImageURL}
+                                            alt='product-image'
+                                            width='100px'
+                                            height='100px'
+                                            className='ml-3 mr-2'
+                                            />
+                                        </Link>
+                                        <p className="text-secondary">
+                                            <span className="float-right">{ new Date(product.DatePosted).getFullYear() }-{ new Date(product.DatePosted).getMonth() + 1 }-{ new Date(product.DatePosted).getDate() }</span>
+                                        </p>
+                                        <p>"{ product.ItemDetails }"</p>
+                                    </div>
+                                </article>)
+                            }
+                        </div>
                     </div>
                 </div>
-            </div>
-            </div>
             </div>
         );
     }
